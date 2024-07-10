cb1eb5e9cbed3b9cc41af1bd1c42135be43281d8
tm267619 <thibault.moulignier@cea.fr>
2024-07-10 14:50:57 +0200

    essai fix setup python 3.7

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from setuptools import find_packages, setup
 
 # To publish :
@@ -8,40 +9,45 @@ from setuptools import find_packages, setup
 # python3 -m twine upload --repository pypi dist/*
 
 setup(
-            name='eesrep',
-            version='0.1.4',
-            description='EESREP is a component based energy system optimisation python module.',
-            long_description='EESREP is a component based energy system optimisation python module.',
-            author='CEA',
-            maintainer="Thibault Moulignier",
-            author_email='Thibault.Moulignier@cea.fr',
-            packages=find_packages(),
-            package_data={'eesrep': ['components/*.py', 'solver_interface/*.py']},
-            url="https://github.com/tmoulignier/EESREP",
-            keywords="optimization, modelling, energy, system",
-            python_requires=">=3.6, <4",
-            install_requires=["pandas", 
-                              "numpy",
-                              "matplotlib"],
-            project_urls={  # Optional
-                  "Bug Reports": "https://github.com/tmoulignier/EESREP/issues",
-                  "Doc": "https://eesrep.readthedocs.io/en/latest//index.html",
-                  "Source": "https://github.com/tmoulignier/EESREP",
-            },
-            extras_require={
-                  'docs-requirements-txt': [
-                        'sphinx',
-                        'sphinx_rtd_theme',
-                        'myst_parser',
-                        'numpydoc',
-                        'nbsphinx',
-                        'ipython',
-                        'jupyter',
-                        'docplex',
-                        'cplex',
-                        'mip',
-                        'pyomo',
-                        'pandoc'
-                  ]
-            }
-      )
+    name='eesrep',
+    version='0.1.4',
+    description='EESREP is a component based energy system optimisation python module.',
+    long_description='EESREP is a component based energy system optimisation python module.',
+    author='CEA',
+    maintainer="Thibault Moulignier",
+    author_email='Thibault.Moulignier@cea.fr',
+    packages=find_packages(),
+    package_data={'eesrep': [
+                                'components/*.py', 
+                                'solver_interface/*.py'
+                             ]},
+    url="https://github.com/tmoulignier/EESREP",
+    keywords="optimization, modelling, energy, system",
+    python_requires=">=3.6, <4",
+    install_requires=[
+        "pandas", 
+        "numpy",
+        "matplotlib"],
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/tmoulignier/EESREP/issues",
+        "Doc": "https://eesrep.readthedocs.io/en/latest//index.html",
+        "Source": "https://github.com/tmoulignier/EESREP",
+        },
+    extras_require={
+        'docs-requirements-txt': [
+            ("setuptools", "<=68.2.2" if sys.version_info[0] == 3 and sys.version_info[1] == 7 else '>= 0.'),
+            'sphinx',
+            'sphinx_rtd_theme',
+            'myst_parser',
+            'numpydoc',
+            'nbsphinx',
+            'ipython',
+            'jupyter',
+            'docplex',
+            'cplex',
+            'mip',
+            'pyomo',
+            'pandoc'
+        ]
+    }
+)
